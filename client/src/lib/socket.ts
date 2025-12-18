5fe1fae90cec9df0f5ac61695150a4a9dedfd803
Haman-to-thapa <hamanthapa0000@gmail.com>
2025-12-18 21:55:15 +0530

    fixed socket io

--- client/src/lib/socket.ts
+++ client/src/lib/socket.ts
@@ -1,1 +1,15 @@
- 
+ import { io, Socket } from "socket.io-client";
+
+let socket: Socket | null = null;
+
+export const getSocket = (): Socket => {
+  if (!socket) {
+    socket = io("https://crud-task-oh51.onrender.com", {
+      transports: ["websocket"],
+      withCredentials: true, 
+      autoConnect: false     
+    });
+  }
+
+  return socket;
+};
