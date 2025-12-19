200f8147427ae45b46b6b96bdb382abdbacc1049
Haman-to-thapa <hamanthapa0000@gmail.com>
2025-12-19 08:11:38 +0530

    fix AuthRequest typing with express module augmentation

--- server/src/types/auth-request.ts
+++ server/src/types/auth-request.ts
@@ -2,4 +2,10 @@ import { Request } from "express";
 
 export interface AuthRequest extends Request {
   userId?: string;
+  body: any;
+  params: any;
+  query: any;
+  cookies: {
+    token?: string;
+  };
 }
